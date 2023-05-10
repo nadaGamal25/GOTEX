54a414c9fc7e2ca73a2fe19d342090565e1df6ac
nada <gnada1221@gmail.com>
2023-05-10 22:35:43 +0200

    updated

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import glt from '../../assets/glt.jpg'
 import imile from '../../assets/imile.jpg'
@@ -9,13 +9,119 @@ import sae from '../../assets/sae.jpg'
 import sms from '../../assets/sms.jpg'
 import spl from '../../assets/spl.jpg'
 import armx from '../../assets/armx.jpg'
+import axios from 'axios'
+import Joi from 'joi'
 
 export default function Admin() {
+  const [errorList, seterrorList]= useState([]); 
+  const [saeePrices,setSaeePrices] =useState({
+    status :'',
+    userprice :'',
+    marketerprice:'',
+    kgprice :'',
+  })
+  const [error , setError]= useState('')
+  const [isLoading, setisLoading] =useState(false)
+
+async function sendSaeePricesToApi(){
+  let {data}= await axios.post(`https://dashboard.go-tex.net/api/saee/edit`,saeePrices);
+  if(data.msg == 'ok'){
+    setisLoading(false)
+    window.alert("تم التسجيل بنجاح")
+    console.log(data)
+
+  }
+  else{
+    setisLoading(false)
+    setError(data.msg)
+  }
+}
+function submitSaeePricesForm(e){
+  e.preventDefault();
+  setisLoading(true)
+  let validation = validateSaeePricesForm();
+  console.log(validation);
+  if(validation.error){
+    setisLoading(false)
+    seterrorList(validation.error.details)
+
+  }else{
+    sendSaeePricesToApi();
+  }
+
+}
+
+  function getSaeePrices(e){
+    let mySaeePrices={...saeePrices};
+    mySaeePrices[e.target.name]= e.target.value;
+    setSaeePrices(mySaeePrices);
+    console.log(mySaeePrices);
+  }
+
+  function validateSaeePricesForm(){
+    let scheme= Joi.object({
+        status:Joi.required(),
+        userprice:Joi.number().required(),
+        marketerprice:Joi.number().required(),
+        kgprice :Joi.number().required()
+
+    });
+    return scheme.validate(saeePrices, {abortEarly:false});
+  }
   return (
     <>
         <div className='p-4 admin' id='content'>
             <h3>أسعار الشركات</h3>
-            <div className="clients-table p-4 mt-4">
+            <div className="row py-3">
+              <div className="col-md-6">
+                <div className="p-saee p-3">
+                  <h5 className="text-center">أسعار شركة ساعي</h5>
+                  <form onSubmit={submitSaeePricesForm} action="">
+                    <label htmlFor="">سعر المسخدم</label>
+                    <input onChange={getSaeePrices} type="number" className='my-input my-2 form-control' name='userprice' />
+                    {errorList.map((err,index)=>{
+      if(err.context.label ==='address'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء جميع البيانات</div>
+      }
+      
+    })}
+                    <label htmlFor="">سعر المتاجر</label>
+                    <input onChange={getSaeePrices} type="number" className='my-input my-2 form-control' name='marketerprice' />
+                    {errorList.map((err,index)=>{
+      if(err.context.label ==='address'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء جميع البيانات</div>
+      }
+      
+    })}
+                    <label htmlFor="">سعر الزيادة</label>
+                    <input onChange={getSaeePrices} type="number" className='my-input my-2 form-control' name='kgprice' />
+                    {errorList.map((err,index)=>{
+      if(err.context.label ==='address'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء جميع البيانات</div>
+      }
+      
+    })}
+                    <label htmlFor="">الحالة</label>
+                    <select className='my-input my-2 form-control' onChange={getSaeePrices} name='status'>
+                      <option></option>
+                      <option>True</option>
+                      <option>False</option>
+                      </select>
+                      {errorList.map((err,index)=>{
+      if(err.context.label ==='address'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء جميع البيانات</div>
+      }
+      
+    })}
+
+                      <button className='btn btn-primary mt-3'>
+                      {isLoading == true?<i class="fa-solid fa-spinner fa-spin"></i>:'تسجيل'}
+                     </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            {/* <div className="clients-table p-4 mt-4">
         <table className="table">
         <thead>
     <tr>
@@ -59,7 +165,7 @@ export default function Admin() {
     </tr>
     </tbody>
         </table>
-      </div>
+      </div> */}
       <div className="shipment-details mt-4 p-4">
         <h3>تفاصيل الشحنات</h3>
         <div className="company-links py-3">
--- src/Components/ShipmentForms/ShipmentForms.jsx
+++ src/Components/ShipmentForms/ShipmentForms.jsx
@@ -2,14 +2,84 @@ import React, { useState } from 'react'
 import PhoneInput from 'react-phone-number-input';
 import 'react-phone-number-input/style.css'
 import ar from 'react-phone-number-input/locale/ar'
+import axios from 'axios';
+import Joi from 'joi';
 
 export default function ShipmentForms() {
     const [value ,setPhoneValue]=useState()
     const [phone2,setPhone2] =useState()
 
+    const [errorList, seterrorList]= useState([]); 
+  const [orderData,setOrderData] =useState({
+    p_name:'',
+    p_city:'',
+    p_mobile:'',
+    p_streetaddress:'',
+    weight:'',
+    quantity:'',
+    c_name:'',
+    c_city:'',
+    c_streetaddress:'',
+    c_mobile:''
+  })
+  const [error , setError]= useState('')
+  const [isLoading, setisLoading] =useState(false)
+
+async function sendOrderDataToApi(){
+  let {data}= await axios.post(`https://dashboard.go-tex.net/api/saee/create-user-order`,orderData);
+  if(data.message == 'success'){
+    setisLoading(false)
+    window.alert("تم التسجيل بنجاح")
+    console.log(data)
+
+  }
+  else{
+    setisLoading(false)
+    setError(data.message)
+  }
+}
+function submitOrderUserForm(e){
+  e.preventDefault();
+  setisLoading(true)
+  let validation = validateOrderUserForm();
+  console.log(validation);
+  if(validation.error){
+    setisLoading(false)
+    seterrorList(validation.error.details)
+
+  }else{
+    sendOrderDataToApi();
+  }
+
+}
+
+  function getOrderData(e){
+    let myOrderData={...orderData};
+    myOrderData[e.target.name]= e.target.value;
+    setOrderData(myOrderData);
+    console.log(myOrderData);
+  }
+
+  function validateOrderUserForm(){
+    let scheme= Joi.object({
+        p_name:Joi.string().required(),
+        p_city:Joi.string().required(),
+        p_mobile:Joi.string().min(12).required(),
+        p_streetaddress:Joi.string().required(),
+        weight:Joi.string().required(),
+        quantity:Joi.string().required(),
+        c_name:Joi.string().required(),
+        c_city:Joi.string().required(),
+        c_streetaddress:Joi.string().required(),
+        c_mobile:Joi.string().min(12).required()
+
+    });
+    return scheme.validate(orderData, {abortEarly:false});
+  }
+
   return (
     <div className='p-4' id='content'>
-        <form className='shipmenForm' action="">
+        <form onSubmit={submitOrderUserForm} className='shipmenForm' action="">
             <div className="row">
             <div className="col-md-6">
             <div className="shipper-details brdr-grey p-4">
@@ -17,24 +87,48 @@ export default function ShipmentForms() {
                 
                 <div className='pb-3'>
                 <label htmlFor=""> اسم الشركة/المتجر</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='p_name' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             <div className='pb-3'>
                 <label htmlFor="">رقم الهاتف</label>
                 {/* <input type="text" className="form-control" /> */}
-                <PhoneInput name='phone' 
+                <PhoneInput name='p_mobile' 
     labels={ar} defaultCountry='EG' className='phoneInput' value={value}
-    onChange={value=>setPhoneValue(value)}/>
+    onChange={value=>setPhoneValue(value)} onClick={getOrderData}/>
+    {errorList.map((err,index)=>{
+      if(err.context.label ==='mobile'){
+        return <div key={index} className="alert alert-danger my-2">رقم الهاتف يجب الا يقل عن 12 رقما</div>
+      }
+      
+    })}
       
             </div>
             <div className='pb-3'>
                 <label htmlFor=""> الموقع</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='p_city' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             <h6 className='text-center py-2'>{'<<'}  معلومات اضافية  {'>>'}</h6>
             <div className='pb-3'>
                 <label htmlFor=""> العنوان </label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='p_streetaddress' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             <div className='pb-3'>
                 <label htmlFor=""> البريد الالكترونى</label>
@@ -47,7 +141,13 @@ export default function ShipmentForms() {
                 <div className="col-md-6">
                 <div className='pb-3'>
                 <label htmlFor=""> الوزن</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='weight' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
                 </div>
                 <div className="col-md-6">
@@ -71,7 +171,13 @@ export default function ShipmentForms() {
                 <div className="col-md-6">
                 <div className='pb-3'>
                 <label htmlFor=""> عدد القطع</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='quantity' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
                 </div>
                 <h6 className='text-center py-2'>{'<<'}  معلومات اضافية  {'>>'}</h6>
@@ -99,28 +205,52 @@ export default function ShipmentForms() {
             <div className="col-md-6">
             <div className="reciever-details brdr-grey p-3">
                 <h3>تفاصيل المستلم</h3>
-                <div className=" mb-4 mt-2">
+                {/* <div className=" mb-4 mt-2">
         <input className='form-control' type="search" placeholder='بحث بالأسم' />
-        </div>
+        </div> */}
         <div className='pb-3'>
                 <label htmlFor=""> الاسم</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='c_name' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             <div className='pb-3'>
                 <label htmlFor=""> رقم الهاتف</label>
                 {/* <input type="text" className="form-control"/> */}
-                <PhoneInput name='phone2' 
+                <PhoneInput name='c_mobile' 
     labels={ar} defaultCountry='EG' className='phoneInput' value={phone2}
-    onChange={phone2=>setPhone2(phone2)}/>
+    onChange={phone2=>setPhone2(phone2)} onClick={getOrderData}/>
+    {errorList.map((err,index)=>{
+      if(err.context.label ==='mobile'){
+        return <div key={index} className="alert alert-danger my-2">رقم الهاتف يجب الا يقل عن 12 رقما</div>
+      }
+      
+    })}
       
             </div>
             <div className='pb-3'>
                 <label htmlFor=""> الموقع</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='c_city' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             <div className='pb-3'>
                 <label htmlFor=""> العنوان</label>
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" name='c_streetaddress' onChange={getOrderData}/>
+                {errorList.map((err,index)=>{
+      if(err.context.label ==='location'){
+        return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة </div>
+      }
+      
+    })}
             </div>
             
             <h6 className='text-center py-2'>{'<<'}  معلومات اضافية  {'>>'}</h6>
