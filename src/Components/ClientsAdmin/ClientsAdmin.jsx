54a27f1126372bc02e4a4759e51432d5ec0eee0c
nada <gnada1221@gmail.com>
2023-10-22 00:29:45 +0200

    p

--- src/Components/ClientsAdmin/ClientsAdmin.jsx
+++ src/Components/ClientsAdmin/ClientsAdmin.jsx
@@ -6,6 +6,11 @@ export default function ClientsAdmin() {
         getClientsAdmin()
       },[])
       const [clientsAdmin,setClientsAdmin]=useState([])
+      const [searchName, setSearchName] = useState('');
+      const [searchEmail, setSearchEmail] = useState('');
+      const [searchPhone, setSearchPhone] = useState('');
+      const [searchCity, setSearchCity] = useState('');
+      const [searchID, setSearchID] = useState('');
     
       async function getClientsAdmin() {
         try {
@@ -22,9 +27,49 @@ export default function ClientsAdmin() {
           console.error(error);
         }
       }
+      const filteredClients = clientsAdmin.filter((item) => {
+        return (
+          (searchPhone === '' || item.Client?.phone1?.includes(searchPhone)) &&
+          (searchName === '' || (item.Client?.first_name?.includes(searchName)) )&&
+          (searchEmail === '' || item.Client?.email?.includes(searchEmail)) &&
+          (searchCity === '' || item.Client?.city.includes(searchCity)) &&
+          (searchID === '' || item.Client?.id?.includes(searchID))
+         
+        );
+    
+      });
   return (
     <>
     <div className='p-5' id='content'>
+    <div className="gray-table p-4 mb-4">
+      <div className="row">
+        
+        <div className="col-md-4">
+          <label htmlFor="">اسم العميل:</label>
+          <input className='form-control' type="search" value={searchName} onChange={(e) => setSearchName(e.target.value)} />
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">الايميل:</label>
+          <br/><input className='form-control' type="search" value={searchEmail} onChange={(e) => setSearchEmail(e.target.value)} />
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">الهاتف:</label>
+          <input className='form-control' type="search" value={searchPhone} onChange={(e) => setSearchPhone(e.target.value)} />
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">المدينة :</label>
+          <input className='form-control' type="search" value={searchCity} onChange={(e) => setSearchCity(e.target.value)} />
+        </div>
+        <div className="col-md-4">
+          <label htmlFor="">id_العميل :</label>
+          <input className='form-control' type="search" value={searchID} onChange={(e) => setSearchID(e.target.value)} />
+        </div>
+       
+        <div className="text-center mt-3">
+          <button className="btn dark"><i class="fa-solid fa-magnifying-glass"></i> بحث</button>
+        </div>
+      </div>
+    </div>
     <div className="clients-table p-4 my-4">
       <table className="table">
         <thead>
@@ -43,7 +88,7 @@ export default function ClientsAdmin() {
           </tr>
         </thead>
         <tbody>
-          {clientsAdmin && clientsAdmin.map((item,index) =>{
+          {filteredClients && filteredClients.map((item,index) =>{
             return(
               <tr key={index}>
                 <td>{index+1}</td>
