fc72f93333fc85870ea397f039d6ae323c92517e
nada <gnada1221@gmail.com>
2024-02-14 16:04:10 +0200

    updated

--- src/Components/AramexShipments/AramexShippments.jsx
+++ src/Components/AramexShipments/AramexShippments.jsx
@@ -1003,7 +1003,9 @@ export default function AramexShippments(userData) {
               {orderData.cod === true && (
     <div className='pb-3'>
       <label htmlFor=""> قيمة الشحنة</label>
-      <input type="number" step="0.001" className="form-control" name='shipmentValue' onChange={getOrderData} required />
+      <input
+      //  type="number" step="0.001" 
+      className="form-control" name='shipmentValue' onChange={getOrderData} required />
       {errorList.map((err, index) => {
         if (err.context.label === 'shipmentValue') {
           return <div key={index} className="alert alert-danger my-2">يجب ملىء هذه الخانة</div>
